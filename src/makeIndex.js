33a4c1069ce99e60eb5b15791725b4a4d795d9bf
Pitworker <sir.button.esq@gmail.com>
2020-12-29 23:59:02 -0500

    functioning tags menu, unformatted. added files to run locally

--- src/makeIndex.js
+++ src/makeIndex.js
@@ -45,18 +45,20 @@ function listAllTags() {
   for (let i = 0; i < content.content.length; i++) {
     let c = content.content[i];
     
-    for (t in c.tags) {
-      if (t == "year" && !tagList.year.includes(c.tags.year)) {
+    for (let t in c.tags) {
+      if (t == "year" && tagList.year.indexOf(c.tags.year) < 0) {
         tagList.year.push(c.tags.year.toString());
       } else if (t != "year" && c.tags[t] != null) {
         for (let j = 0; j < c.tags[t].length; j++) {
-          tagList[t].push(c.tags[t][j]);
+          if (tagList[t].indexOf(c.tags[t][j]) < 0) {
+	    tagList[t].push(c.tags[t][j]);
+	  }
         }
       }
     }
   }
   
-  for (t in tagList) {
+  for (let t in tagList) {
     tagList[t].sort();
   }
   
@@ -65,17 +67,19 @@ function listAllTags() {
 
 function parseTags(h) {
   let tags = [];
-  let subH = h.substring(1);
-  let i = subH.indexOf('+');
-  while (i > 0) {
-    tags.push(subH.substring(0,i).replace(/-/g, ' '));
-    subH = subH.substring(i + 1);
-    i = subH.indexOf('+');
-  }
-  tags.push(subH.replace(/-/g, ' '));
 
-  tags.sort();
-  
+  if (h != "" && h != "#" && h != undefined) {
+    let subH = h.substring(1);
+    let i = subH.indexOf('+');
+    while (i > 0) {
+      tags.push(subH.substring(0,i).replace(/-/g, ' '));
+      subH = subH.substring(i + 1);
+      i = subH.indexOf('+');
+    }
+    tags.push(subH.replace(/-/g, ' '));
+    
+    tags.sort();
+  }
   return tags;
 }
 
@@ -92,8 +96,8 @@ function labelTags(t,allTags) {
   for (let i = 0; i < t.length; i++) {
     let foundTag = false;
 
-    for (j in allTags) {
-      if (allTags[j].includes(t[i])) {
+    for (let j in allTags) {
+      if (allTags[j].indexOf(t[i]) >= 0) {
         labelT[j].push(t[i]);
         foundTag = true;
         break;
@@ -108,12 +112,98 @@ function labelTags(t,allTags) {
   return labelT;
 }
 
+function writeHash(tags) {
+  let hash = "#";
+  let scroll = document.documentElement.scrollTop;
+  for (let j = 0; j < tags.length; j++) {
+    hash = hash + (j < 1 ? "" : "+") + tags[j].replace(/ /g, '-'); 
+  }
+  location.hash = hash;
+  document.documentElement.scrollTop = scroll;
+}
+
+function removeTag(t) {
+  let tags = parseTags(location.hash);
+
+  let i = tags.indexOf(t);
+  if (i >= 0) {
+    tags.splice(i,1);
+    writeHash(tags);
+  }
+
+  console.log("removeTag " + t);
+}
+
+function addTag(t) {
+  let tags = parseTags(location.hash);
+
+  let i = tags.indexOf(t);
+  if (i < 0) {
+    tags.push(t);
+    writeHash(tags);
+  }
+
+  console.log("addTag " + t);
+}
+
 function clearPage() {
   let c = document.getElementById("content");
+  let m = document.getElementById("sideNav");
   if (c != null) {
     while (c.hasChildNodes()) {
       c.removeChild(c.lastChild);
     }
+  }
+  if (m != null) {
+    while (m.hasChildNodes()) {
+      m.removeChild(m.lastChild);
+    }
+  }
+}
+
+function generateTagList(tags,allTags,c) {
+  let menu = document.getElementById("sideNav");
+
+  let closeBtn = document.createElement("BUTTON");
+  closeBtn.className = "closebtn";
+  closeBtn.onclick = function() {closeTags();};
+  closeBtn.innerHTML = "&#215;";
+
+  menu.appendChild(closeBtn);
+  
+  let tagSections = {
+    year: document.createElement("DIV"),
+    course: document.createElement("DIV"),
+    people: document.createElement("DIV"),
+    company: document.createElement("DIV"),
+    skills: document.createElement("DIV"),
+    categories: document.createElement("DIV")
+  };
+
+  for (let t in tagSections) {
+    tagSections[t].id = t + "Menu";
+    let title = document.createTextNode(
+      (t == "categories") ? "Tags" :
+        t.charAt(0).toUpperCase() + t.slice(1));
+
+    tagSections[t].appendChild(title);
+
+    for (let i = 0; i < allTags[t].length; i++) {
+      let btn = document.createElement("BUTTON");
+      if (tags[t].indexOf(allTags[t][i]) >= 0) {
+        btn.id = t + "Selected";
+        btn.onclick = function() {removeTag(allTags[t][i]);};
+        btn.innerHTML = "&nbsp;" + allTags[t][i] + "&nbsp;&#215;&nbsp;";
+      } else {
+        btn.id = t + "Unselected";
+        btn.onclick = function() {addTag(allTags[t][i]);};
+        btn.innerHTML = "&nbsp;" + allTags[t][i] + "&nbsp;";
+      }
+      tagSections[t].appendChild(document.createElement("BR"));
+      tagSections[t].appendChild(btn);
+    }
+
+    menu.appendChild(tagSections[t]);
   }
 }
 
@@ -131,18 +221,13 @@ function generateTags(t,n) {
   let f = document.createElement("SPAN");
   f.id = "tagsMod";
   
-  let fBtn = document.createElement("A");
-  fBtn.href = "javascript:openTags()";
-  fBtn.innerHTML = "&#8942; Filters ";
+  let fBtn = document.createElement("BUTTON");
+  fBtn.onclick = function() {openTags();};
+  fBtn.innerHTML = "&nbsp;&#8942; Filters&nbsp;";
 
   f.appendChild(fBtn);
   c.appendChild(f);
-  
-  /*let fTitle = document.createElement("SPAN");
-  fTitle.id = "tagsTitle";
-  fTitle.innerText =  " Filters ";
-
-  c.appendChild(fTitle);*/
+  c.appendChild(document.createTextNode(" "));
   
   let tags = {
     year: {
@@ -207,6 +292,10 @@ function generateTags(t,n) {
   n.appendChild(r);
 }
 
+function generateTiles(t,c) {
+
+}
+
 function generateCopyright(n) {
   let e = document.createElement("DIV");
   e.className = "row";
@@ -225,9 +314,29 @@ function generateCopyright(n) {
   n.appendChild(e);
 }
 
+function openTags() {
+  document.getElementById("sideNav").style.width =
+    screen.width > 600 ? "400px" : "100%";
+  document.getElementById("sideNav").style.outlineStyle =
+    screen.width > 600 ? "solid" : "none";
+  if (screen.width > 600) {
+    document.getElementById("sideNav").style.outlineColor = "#000000";
+    document.getElementById("sideNav").style.outlineWidth = "3px";
+  }
+}
+
+function closeTags() {
+  document.getElementById("sideNav").style.width = "0";
+  document.getElementById("sideNav").style.outline = "none";
+  //document.getElementById("main").style.marginLeft = "0";
+}
+
 function populateIndexPage(t, allTags) {
   let c = document.getElementById("content");
-  generateTags(labelTags(t,allTags),c);
+  let labelT = labelTags(t,allTags);
+  generateTagList(labelT,allTags,c);
+  generateTags(labelT,c);
+  generateTiles(labelT,c);
   generateCopyright(c);
 }
 
