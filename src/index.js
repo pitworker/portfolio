fc20f68259a9ce1544773ca6d9958d4a5bcaa980
pitworker <swanio@duck.com>
2023-10-17 14:26:10 -0400

    Committing local stuff

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route,
-         useNavigate, useLocation } from 'react-router-dom';
-import { QueryParamProvider } from 'use-query-params';
-import './index.css';
-import Work from './pages/work';
-import Layout from './pages/layout';
-import NoPage from './pages/nopage';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  useLocation
+} from "react-router-dom";
+import { QueryParamProvider } from "use-query-params";
+import "./index.css";
+import Work from "./pages/work";
+import Layout from "./pages/layout";
+import NoPage from "./pages/nopage";
 
 const RouteAdapter = ({ children }) => {
   const navigate = useNavigate();
@@ -41,7 +46,7 @@ export default function App() {
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
