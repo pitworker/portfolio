0e3351a4d8f5a054c769128853e6f7800b62aae5
pitworker <swanio@duck.com>
2023-10-19 22:20:01 -0400

    Parsing Markdown content

--- src/pages/WorkContent.js
+++ src/pages/WorkContent.js
@@ -1,12 +1,49 @@
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ReactDom from "react-dom";
+import Markdown from "react-markdown";
 
-export default function WorkContent () {
-  let { contentId } = useParams();
+import content from "../content/content.json";
+
+const formatFailure = (contentId) => {
+  return `# Oops! \nUnable to load Markdown for ${contentId}`;
+};
+
+const RenderMarkdown = (contentId) => {
+  const [ workContent, setWorkContent ] = useState();
+
+  useEffect(() => {
+    const contentURL = `${window.location.origin}/content/${contentId}.md`;
+    fetch(contentURL).then((response) => {
+      return response.text();
+    }).then((contentString) => {
+      console.log("Received content with body:", contentString);
+      setWorkContent(contentString);
+    }).catch((error) => {
+      console.log(`Error fetching content for ${contentId}:`, error);
+      setWorkContent(formatFailure(contentId));
+    });
+  }, []);
 
   return (
     <div className="work-content">
-      <h1> Work!! </h1>
-      <h3> {`Here's some work for ${contentId}!`} </h3>
+      <Markdown>{ workContent }</Markdown>
     </div>
-  )
+  );
+};
+
+export default function WorkContent () {
+  const { contentId } = useParams();
+
+  for (let contentItem of content.work) {
+    if (contentItem.id === contentId) {
+      return RenderMarkdown(contentId);
+    }
+  }
+
+  return (
+    <div className="work-content">
+      <Markdown>{ formatFailure(contentId) }</Markdown>
+    </div>
+  );
 };
