faacc5bce777bf0a22327a6f2253996b77767988
pitworker <swanio@duck.com>
2023-10-23 10:58:56 -0400

    Updating work content on URL change

--- src/pages/WorkContent.js
+++ src/pages/WorkContent.js
@@ -1,7 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { visit } from "unist-util-visit";
-import ReactDom from "react-dom";
 import Markdown from "react-markdown";
 import remarkDirective from "remark-directive";
 
@@ -46,57 +45,54 @@ const vimeoEmbed = () => {
   };
 };
 
-const FormatFailure = (contentId) => {
-  return `# Oops! \nUnable to load Markdown for ${contentId}`;
+const formatFailure = (contentId) => {
+  return `# Oops! \nUnable to load content for ${contentId}`;
 };
 
-const RenderMarkdown = (contentId) => {
-  const [ workInnerContent, setWorkInnerContent ] = useState();
+const formatLoading = (contentId) => {
+  return `# Please Wait \nContent is loading for ${contentId}`;
+};
 
-  useEffect(() => {
+const loadMarkdown = (contentId) => {
+  return new Promise((resolve, reject) => {
     const contentURL = `${window.location.origin}/content/${contentId}.md`;
     fetch(contentURL).then((response) => {
       return response.text();
     }).then((contentString) => {
       console.log("Received content with body:", contentString);
-      setWorkInnerContent(contentString);
+      resolve(contentString);
     }).catch((error) => {
       console.log(`Error fetching content for ${contentId}:`, error);
-      setWorkInnerContent(FormatFailure(contentId));
+      reject(error);
     });
-  }, []);
-
-  return (
-    <Markdown remarkPlugins={[remarkDirective, vimeoEmbed]}>{
-      workInnerContent
-    }</Markdown>
-  );
+  });
 };
 
 const WorkContent = () => {
-  const [ workContent, setWorkContent ] = useState();
-  let { contentId } = useParams();
+  const { contentId } = useParams();
+  const [ workContent, setWorkContent ] = useState(formatLoading(contentId));
+  const contentDidLoad = useRef(false);
 
   for (let contentItem of content.work) {
     if (contentItem.id === contentId) {
-      setWorkContent(RenderMarkdown(contentId));
+      loadMarkdown(contentId).then((response) => {
+        setWorkContent(response);
+      }).catch((error) => {
+        setWorkContent(formatFailure(contentId));
+      });
+      contentDidLoad.current = true;
+      break;
     }
   }
-  setWorkContent(<Markdown>{FormatFailure(contentId)}</Markdown>);
 
-  /*
-  useEffect(() => {
-    for (let contentItem of content.work) {
-      if (contentItem.id === contentId) {
-        setWorkContent(RenderMarkdown(contentId));
-      }
-    }
-    setWorkContent(<Markdown>{FormatFailure(contentId)}</Markdown>);
-  }, [contentId]);
-  */
+  if (!contentDidLoad.current) setWorkContent(formatFailure(contentId));
 
   return (
-    <div className="work-content"> { workContent } </div>
+    <div className="work-content">
+      <Markdown remarkPlugins={ [ remarkDirective, vimeoEmbed ] }>
+        { workContent }
+      </Markdown>
+    </div>
   );
 };
 
